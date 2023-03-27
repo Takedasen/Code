82a2b0d718085bb2cabeb631912a704cca3c39d8
Ronin <105971705+Takedasen@users.noreply.github.com>
2023-03-27 17:06:29 +0300

    Update 27.03.2023

--- Ronin Clock/script.js
+++ Ronin Clock/script.js
@@ -3,22 +3,22 @@ function showTime(){
     var h = date.getHours(); // 0 - 23
     var m = date.getMinutes(); // 0 - 59
     var s = date.getSeconds(); // 0 - 59
-    var session = "AM";
+    var session = "";
     
     if(h == 0){
-        h = 12;
+        h = 24;
     }
     
-    if(h > 12){
-        h = h - 12;
-        session = "PM";
+    if(h > 24){
+        h = h - 24;
+        session = "";
     }
     
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
     
-    var time = h + ":" + m + ":" + s + " " + session;
+    var time = h + ":" + m + ":" + s + "" + session;
     document.getElementById("MyClockDisplay").innerText = time;
     document.getElementById("MyClockDisplay").textContent = time;
     

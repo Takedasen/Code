05495929220bfafab351fff036834d5221635c89
Ronin <105971705+Takedasen@users.noreply.github.com>
2023-04-29 15:05:23 +0300

    29.04.2023 Changes

--- PYTHON/Test_Task_Maclay/Test_Task.py
+++ PYTHON/Test_Task_Maclay/Test_Task.py
@@ -1,19 +1,87 @@
 {
     "paraphrases": [
         {
-            "tree": "(S (NP (NP (DT The) (JJ charming) (NNP Gothic) (NNP Quarter)) (, ,) (CC or) (NP (NNP Barri) (NNP Gòtic))) (, ,) (VP (VBZ has) (NP (NP (JJ narrow) (JJ medieval) (NNS streets)) (VP (VBN filled) (PP (IN with) (NP (NP (NNS clubs)) (, ,) (NP (JJ trendy) (NNS bars)) (CC and) (NP (JJ Catalan) (NNS restaurants))))))))
+            "tree": "(S (NP (NP (DT The) (JJ charming) (NNP Gothic) (NNP Quarter)) (, ,) (CC or) (NP (NNP Barri) (NNP Gòtic))) 
+            (, ,) (VP (VBZ has) (NP (NP (JJ narrow) (JJ medieval) (NNS streets)) (VP (VBN filled) (PP (IN with) (NP (NP (NNS clubs)) (, ,) (NP (JJ trendy) 
+            (NNS bars)) (CC and) (NP (JJ Catalan) (NNS restaurants))))))))
         },
         {
-            "tree": "(S (NP (NP (DT The) (JJ charming) (NNP Gothic) (NNP Quarter)) (, ,) (CC or) (NP (NNP Barri) (NNP Gòtic))) (, ,) (VP (VBZ has) (NP (NP (JJ narrow) (JJ medieval) (NNS streets)) (VP (VBN filled) (PP (IN with) (NP (NP (JJ trendy) (NNS bars)) (, ,) (NP (JJ Catalan) (NNS restaurants)) (CC and) (NP (NNS clubs))))))))
+            "tree": "(S (NP (NP (DT The) (JJ charming) (NNP Narcotic) (NNP Quarter)) (, ,) (CC or) (NP (NNP Barri) (NNP Gòtic))) 
+                      (, ,) (VP (VBZ has) (NP (NP (JJ narrow) (JJ medieval) (NNS streets)) (VP (VBN filled) (PP (IN with) (NP (NP (JJ trendy) (NNS bars)) 
+                                                                                                                           (, ,) (NP (JJ Catalan) (NNS restaurants)) (CC and) (NP (NNS clubs))))))))
         },
         {
-            "tree": "(S (NP (NP (DT The) (JJ charming) (NNP Gothic) (NNP Quarter)) (, ,) (CC or) (NP (NNP Barri) (NNP Gòtic))) (, ,) (VP (VBZ has) (NP (NP (JJ narrow) (JJ medieval) (NNS streets)) (VP (VBN filled) (PP (IN with) (NP (NP (JJ Catalan) (NNS restaurants)) (, ,) (NP (NNS clubs)) (CC and) (NP (JJ trendy) (NNS bars))))))))
+            "tree": "(S (NP (NP (DT The) (JJ charming) (NNP Sybiotic) (NNP Quarter)) (, ,) (CC or) (NP (NNP Barri) (NNP Gòtic))) 
+            (, ,) (VP (VBZ has) (NP (NP (JJ narrow) (JJ medieval) (NNS streets)) (VP (VBN filled) (PP (IN with) (NP (NP (JJ Catalan) (NNS restaurants)) 
+            (, ,) (NP (NNS clubs)) (CC and) (NP (JJ trendy) (NNS bars))))))))
         },
         {
-            "tree": "(S (NP (NP (DT The) (JJ charming) (NNP Gothic) (NNP Quarter)) (, ,) (CC or) (NP (NNP Barri) (NNP Gòtic))) (, ,) (VP (VBZ has) (NP (NP (JJ narrow) (JJ medieval) (NNS streets)) (VP (VBN filled) (PP (IN with) (NP (NP (NNS clubs)) (, ,) (NP (JJ Catalan) (NNS restaurants)) (CC and) (NP (JJ trendy) (NNS bars))))))))
+            "tree": "(S (NP (NP (DT The) (JJ charming) (NNP Flurotic) (NNP Quarter)) (, ,) (CC or) (NP (NNP Barri) (NNP Gòtic))) 
+                      (, ,) (VP (VBZ has) (NP (NP (JJ narrow) (JJ medieval) (NNS streets)) (VP (VBN filled) (PP (IN with) (NP (NP (NNS clubs)) (, ,)
+(NP (JJ Catalan) (NNS restaurants)) (CC and) (NP (JJ trendy) (NNS bars))))))))
         },
         {
-            "tree": "(S (NP (NP (DT The) (JJ charming) (NNP Gothic) (NNP Quarter)) (, ,) (CC or) (NP (NNP Barri) (NNP Gòtic))) (, ,) (VP (VBZ has) (NP (NP (JJ narrow) (JJ medieval) (NNS streets)) (VP (VBN filled) (PP (IN with) (NP (NP (JJ Catalan) (NNS restaurants)) (, ,) (NP (JJ trendy) (NNS bars)) (CC and) (NP (NNS clubs))))))))
+            "tree": "(S (NP (NP (DT The) (JJ charming) (NNP Galotic) (NNP Quarter)) (, ,) (CC or) (NP (NNP Barri) (NNP Gòtic))) 
+            (, ,) (VP (VBZ has) (NP (NP (JJ narrow) (JJ medieval) (NNS streets)) (VP (VBN filled) (PP (IN with) (NP (NP (JJ Catalan) (NNS restaurants)) 
+            (, ,) (NP (JJ trendy) (NNS bars)) (CC and) (NP (NNS clubs))))))))
+        }
+    ]
+}
+{
+    "paraphrases": [
+        {
+            "tree": "(S (NP (NP (DT The) (JJ charming) (NNP Gothic) (NNP Quarter)) (, ,) (CC or) (NP (NNP Barri) (NNP Gòtic))) 
+            (, ,) (VP (VBZ has) (NP (NP (JJ narrow) (JJ medieval) (NNS streets)) (VP (VBN filled) (PP (IN with) (NP (NP (NNS clubs)) (, ,) (NP (JJ trendy) 
+            (NNS bars)) (CC and) (NP (JJ Catalan) (NNS restaurants))))))))
+        },
+        {
+            "tree": "(S (NP (NP (DT The) (JJ charming) (NNP Narcotic) (NNP Quarter)) (, ,) (CC or) (NP (NNP Barri) (NNP Gòtic))) 
+                      (, ,) (VP (VBZ has) (NP (NP (JJ narrow) (JJ medieval) (NNS streets)) (VP (VBN filled) (PP (IN with) (NP (NP (JJ trendy) (NNS bars)) 
+                                                                                                                           (, ,) (NP (JJ Catalan) (NNS restaurants)) (CC and) (NP (NNS clubs))))))))
+        },
+        {
+            "tree": "(S (NP (NP (DT The) (JJ charming) (NNP Sybiotic) (NNP Quarter)) (, ,) (CC or) (NP (NNP Barri) (NNP Gòtic))) 
+            (, ,) (VP (VBZ has) (NP (NP (JJ narrow) (JJ medieval) (NNS streets)) (VP (VBN filled) (PP (IN with) (NP (NP (JJ Catalan) (NNS restaurants)) 
+            (, ,) (NP (NNS clubs)) (CC and) (NP (JJ trendy) (NNS bars))))))))
+        },
+        {
+            "tree": "(S (NP (NP (DT The) (JJ charming) (NNP Flurotic) (NNP Quarter)) (, ,) (CC or) (NP (NNP Barri) (NNP Gòtic))) 
+                      (, ,) (VP (VBZ has) (NP (NP (JJ narrow) (JJ medieval) (NNS streets)) (VP (VBN filled) (PP (IN with) (NP (NP (NNS clubs)) (, ,)
+(NP (JJ Catalan) (NNS restaurants)) (CC and) (NP (JJ trendy) (NNS bars))))))))
+        },
+        {
+            "tree": "(S (NP (NP (DT The) (JJ charming) (NNP Galotic) (NNP Quarter)) (, ,) (CC or) (NP (NNP Barri) (NNP Gòtic))) 
+            (, ,) (VP (VBZ has) (NP (NP (JJ narrow) (JJ medieval) (NNS streets)) (VP (VBN filled) (PP (IN with) (NP (NP (JJ Catalan) (NNS restaurants)) 
+            (, ,) (NP (JJ trendy) (NNS bars)) (CC and) (NP (NNS clubs))))))))
+        }
+    ]
+}
+{
+    "paraphrases": [
+        {
+            "tree": "(S (NP (NP (DT The) (JJ charming) (NNP Gothic) (NNP Quarter)) (, ,) (CC or) (NP (NNP Barri) (NNP Gòtic))) 
+            (, ,) (VP (VBZ has) (NP (NP (JJ narrow) (JJ medieval) (NNS streets)) (VP (VBN filled) (PP (IN with) (NP (NP (NNS clubs)) (, ,) (NP (JJ trendy) 
+            (NNS bars)) (CC and) (NP (JJ Catalan) (NNS restaurants))))))))
+        },
+        {
+            "tree": "(S (NP (NP (DT The) (JJ charming) (NNP Narcotic) (NNP Quarter)) (, ,) (CC or) (NP (NNP Barri) (NNP Gòtic))) 
+                      (, ,) (VP (VBZ has) (NP (NP (JJ narrow) (JJ medieval) (NNS streets)) (VP (VBN filled) (PP (IN with) (NP (NP (JJ trendy) (NNS bars)) 
+                                                                                                                           (, ,) (NP (JJ Catalan) (NNS restaurants)) (CC and) (NP (NNS clubs))))))))
+        },
+        {
+            "tree": "(S (NP (NP (DT The) (JJ charming) (NNP Sybiotic) (NNP Quarter)) (, ,) (CC or) (NP (NNP Barri) (NNP Gòtic))) 
+            (, ,) (VP (VBZ has) (NP (NP (JJ narrow) (JJ medieval) (NNS streets)) (VP (VBN filled) (PP (IN with) (NP (NP (JJ Catalan) (NNS restaurants)) 
+            (, ,) (NP (NNS clubs)) (CC and) (NP (JJ trendy) (NNS bars))))))))
+        },
+        {
+            "tree": "(S (NP (NP (DT The) (JJ charming) (NNP Flurotic) (NNP Quarter)) (, ,) (CC or) (NP (NNP Barri) (NNP Gòtic))) 
+                      (, ,) (VP (VBZ has) (NP (NP (JJ narrow) (JJ medieval) (NNS streets)) (VP (VBN filled) (PP (IN with) (NP (NP (NNS clubs)) (, ,)
+(NP (JJ Catalan) (NNS restaurants)) (CC and) (NP (JJ trendy) (NNS bars))))))))
+        },
+        {
+            "tree": "(S (NP (NP (DT The) (JJ charming) (NNP Galotic) (NNP Quarter)) (, ,) (CC or) (NP (NNP Barri) (NNP Gòtic))) 
+            (, ,) (VP (VBZ has) (NP (NP (JJ narrow) (JJ medieval) (NNS streets)) (VP (VBN filled) (PP (IN with) (NP (NP (JJ Catalan) (NNS restaurants)) 
+            (, ,) (NP (JJ trendy) (NNS bars)) (CC and) (NP (NNS clubs))))))))
         }
     ]
 }

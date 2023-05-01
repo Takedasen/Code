f2a46aa2803abc8826579633b222ec727c4e8617
Ronin <105971705+Takedasen@users.noreply.github.com>
2023-05-01 16:37:10 +0300

    Cleanup

--- PYTHON/WeatherAPP/WeatherApp_Test.py
+++ PYTHON/WeatherAPP/WeatherApp_Test.py
@@ -1,62 +1,76 @@
 import requests
 from datetime import datetime
-from tkinter import *
+import tkinter as tk
 
-root = Tk()
+API_KEY = "bda0bb1159cd72004aa8f820b2d18065"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+KELVIN = 273.15
+
+
+def format_time_for_location(utc_with_tz):
+    local_time = datetime.utcfromtimestamp(utc_with_tz)
+    return local_time.time()
+
+
+def get_weather_info(city_name):
+    weather_url = f"{BASE_URL}?q={city_name}&appid={API_KEY}"
+    response = requests.get(weather_url)
+    return response.json()
+
+
+def format_weather_info(weather_info, city_name):
+    if weather_info["cod"] != 200:
+        return f"Weather in '{city_name}' not found!\nPlease enter a valid city name."
+
+    temp = int(weather_info["main"]["temp"] - KELVIN)
+    feels_like_temp = int(weather_info["main"]["feels_like"] - KELVIN)
+    pressure = weather_info["main"]["pressure"]
+    humidity = weather_info["main"]["humidity"]
+    wind_speed = weather_info["wind"]["speed"] / 3.6
+    sunrise = weather_info["sys"]["sunrise"]
+    sunset = weather_info["sys"]["sunset"]
+    timezone = weather_info["timezone"]
+    cloudy = weather_info["clouds"]["all"]
+    description = weather_info["weather"][0]["description"]
+    sunrise_time = format_time_for_location(sunrise + timezone)
+    sunset_time = format_time_for_location(sunset + timezone)
+
+    return (
+        f"Weather in: {city_name}\n\n"
+        f"Temperature: {temp}°C\n"
+        f"Feels like: {feels_like_temp}°C\n"
+        f"Pressure: {pressure} hPa\n"
+        f"Humidity: {humidity}%\n"
+        f"Wind Speed: {wind_speed:.2f} m/s\n"
+        f"Sunrise: {sunrise_time}\n"
+        f"Sunset: {sunset_time}\n"
+        f"Clouds: {cloudy}%\n"
+        f"Additional Info: {description}"
+    )
+
+
+def show_weather():
+    city_name = city_value.get()
+    weather_info = get_weather_info(city_name)
+    weather = format_weather_info(weather_info, city_name)
+    tfield.delete("1.0", "end")
+    tfield.insert("1.0", weather)
+
+
+root = tk.Tk()
 root.geometry("800x650")
 root.resizable(0, 0)
 root.config(bg="#0a0a0a")
 root.title("Weather App")
 
-city_value = StringVar()
+city_value = tk.StringVar()
 
-
-def time_format_for_location(utc_with_tz):
-    local_time = datetime.utcfromtimestamp(utc_with_tz)
-    return local_time.time()
-
-
-def show_weather():
-    api_key = "bda0bb1159cd72004aa8f820b2d18065"
-    city_name = city_value.get()
-    weather_url = (
-        "http://api.openweathermap.org/data/2.5/weather?q="
-        + city_name
-        + "&appid="
-        + api_key
-    )
-    response = requests.get(weather_url)
-    weather_info = response.json()
-    tfield.delete("1.0", "end")
-    if weather_info["cod"] == 200:
-        kelvin = 273.15
-        temp = int(weather_info["main"]["temp"] - kelvin)
-        feels_like_temp = int(weather_info["main"]["feels_like"] - kelvin)
-        pressure = weather_info["main"]["pressure"]
-        humidity = weather_info["main"]["humidity"]
-        wind_speed = weather_info["wind"]["speed"] / 3.6
-        sunrise = weather_info["sys"]["sunrise"]
-        sunset = weather_info["sys"]["sunset"]
-        timezone = weather_info["timezone"]
-        cloudy = weather_info["clouds"]["all"]
-        description = weather_info["weather"][0]["description"]
-        sunrise_time = time_format_for_location(sunrise + timezone)
-        sunset_time = time_format_for_location(sunset + timezone)
-        weather = f"Weather in: {city_name}\n\nTemperature: {temp}°C\nFeels like: {feels_like_temp}°C\nPressure: {pressure} hPa\nHumidity: {humidity}%\nWind Speed: {wind_speed:.2f} m/s\nSunrise: {sunrise_time}\nSunset: {sunset_time}\nClouds: {cloudy}%\nAdditional Info: {description}"
-    else:
-        weather = (
-            f"Weather in '{city_name}' not found!\nPlease enter a valid city name."
-        )
-    tfield.insert(INSERT, weather)
-
-
-#! Interface
-city_head = Label(
+city_head = tk.Label(
     root, text="Enter a city", font="Calibri 18 bold", bg="#0a0a0a", fg="white"
 )
 city_head.pack(pady=20)
 
-inp_city = Entry(
+inp_city = tk.Entry(
     root,
     textvariable=city_value,
     width=24,
@@ -67,9 +81,8 @@ inp_city = Entry(
 )
 inp_city.pack()
 
-Button(
+check_weather_button = tk.Button(
     root,
-    command=show_weather,
     text="Check Weather",
     font="Calibri 18 bold",
     bg="red",
@@ -77,14 +90,16 @@ Button(
     activebackground="white",
     padx=10,
     pady=10,
-).pack(pady=30)
+    command=show_weather,
+)
+check_weather_button.pack(pady=30)
 
-weather_now = Label(
+weather_now = tk.Label(
     root, text="Current Weather Info", font="Calibri 18 bold", bg="#0a0a0a", fg="white"
 )
 weather_now.pack(pady=10)
 
-tfield = Text(
+tfield = tk.Text(
     root,
     width=60,
     height=11,
@@ -94,6 +109,7 @@ tfield = Text(
     padx=10,
     pady=10,
 )
+
 tfield.pack()
 
 root.bind("<Return>", lambda event: show_weather())
